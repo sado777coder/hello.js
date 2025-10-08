8c544f841275c768635211dbdcce12ae2fdde42d
sado <sottieamosofori12@gmail.com>
2025-10-08 14:20:07 -0700

    Initial commit with intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+//intro.js
+const name = "Amos Ofori Sottie";
+const age = "26";
+const favoriteLanguage = "Javascript";
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Favorite Programming Language:",favoriteLanguage);
